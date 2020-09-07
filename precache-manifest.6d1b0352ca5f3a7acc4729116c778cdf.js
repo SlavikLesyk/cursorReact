39c851764a7635d16c49fc9deb73d1ecb9327998
Yaroslav Lesyk <slaviklesyk706@gmail.com>
2020-09-07 15:26:09 +0300

    Updates

---
+++ precache-manifest.6d1b0352ca5f3a7acc4729116c778cdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1288bf26db8e6a1ba817e30b69594a31",
+    "url": "/cursorReact15/index.html"
+  },
+  {
+    "revision": "712961d56ad38c0d149f",
+    "url": "/cursorReact15/static/css/main.7be747f3.chunk.css"
+  },
+  {
+    "revision": "9896674a678f84cd025b",
+    "url": "/cursorReact15/static/js/2.b84fdd21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cursorReact15/static/js/2.b84fdd21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "712961d56ad38c0d149f",
+    "url": "/cursorReact15/static/js/main.d98758e5.chunk.js"
+  },
+  {
+    "revision": "dde406d7633cb2b8f0f1",
+    "url": "/cursorReact15/static/js/runtime-main.989921cf.js"
+  }
+]);
